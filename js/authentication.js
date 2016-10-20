10c20cb97a946c86409d42276f8c06baaeb8f912
nelsonabieno <nelsonabieno@gmail.com>
2016-10-20 16:22:04 -0700

    wrapping up validation class

--- js/authentication.js
+++ js/authentication.js
@@ -2,8 +2,27 @@
  * An authentication class
  */
 
-function Authenticate (){
-
+function Authenticate (username,password){
+    this.username=username;
+    this.password=password;
  }
 
-Authenticate.prototype
+/**
+ * A function validateUser
+ * @param: username,password
+ */
+Authenticate.prototype.validateUser= function(user,pass){
+
+   if (typeof user ==="string" && typeof pass ==="string"){
+
+       if (user===this.username && pass===this.password){
+           return true;
+       }
+       else{
+           return false;
+       }
+   }
+    else{
+       return false;
+   }
+}
